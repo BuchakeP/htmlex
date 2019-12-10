6e15c9e6f30721610d4dde049773df76ec5d9cd8
Your Name <you@example.com>
2019-12-10 05:36:07 -0800

    added

--- hi.js
+++ hi.js
@@ -1,10 +1,6 @@
 var http = require('http');
 
-
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
-}).listen(8080); 
-console.log(printed);
-console.log(hi branch2);
+  res.end('Hello World!');
+}).listen(3000);
